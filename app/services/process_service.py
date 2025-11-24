b70d36a44263ee7a94f160d0ae121586898803fa
siwikmktwl <antoni.rodawski@gmail.com>
2025-11-24 16:51:56 +0100

    Update process_service.py

--- app/services/process_service.py
+++ app/services/process_service.py
@@ -1,5 +1,5 @@
 import os
-import numpy as np
+
 from algorithms.event_shuffle import event_shuffle
 from algorithms.frame_shuffle import frame_shuffle
 from algorithms.frame_blur import frame_blur
@@ -16,24 +16,16 @@ def process_uploaded_file(file, algorithm, form):
     filepath = os.path.join(UPLOAD_FOLDER, file.filename)
     file.save(filepath)
 
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))   
-    APP_DIR = os.path.dirname(BASE_DIR)                  
-    PROCESSED_DIR = os.path.join(APP_DIR, "processed")
-
-    os.makedirs(PROCESSED_DIR, exist_ok=True)
-
     if algorithm == "event_shuffle":
         interval = float(form.get("interval", 50))
+
         raw_out = event_shuffle(filepath, interval=interval)
 
         filename = os.path.basename(raw_out)
-        outpath = os.path.join(PROCESSED_FOLDER, filename)
 
-        data = np.loadtxt(raw_out)
-        np.savetxt(outpath, data, delimiter=',')
+        outpath = os.path.join("app", PROCESSED_FOLDER, filename)
 
         return outpath
-
 
     if algorithm == "frame_shuffle":
         return frame_shuffle(filepath)
@@ -43,4 +35,3 @@ def process_uploaded_file(file, algorithm, form):
         return frame_blur(filepath, sigma=sigma)
 
     raise ValueError("Invalid algorithm")
-
